c9eddced8d497d3e11d335aa5f62c23755f0eaeb
Malinda <malinda555@gmail.com>
2016-06-10 21:28:01 +0530

    initialization project

---
+++ Readme.md
@@ -0,0 +1,50 @@
+# Java SMS sample app for Ideabiz
+
+Sample sms sending and recieving JAVA app using spring and maven. (Modification of (https://github.com/ideabizlk/IdeaBiz-Request-Handler-JAVA/tree/master/Sample)
+
+### Flow
+* Maven install bellow projects
+
+	Ideabiz-APICallHandler (https://github.com/ideabizlk/IdeaBiz-Request-Handler-JAVA)
+	
+	ideabiz-common-java-class (https://github.com/ideabizlk/Classes-JAVA)
+	
+	ideabiz-sms-handler (https://github.com/ideabizlk/IdeaBiz-SMS-API-Handler-JAVA)
+	
+* Create mysql DB and run `Database.sql`
+* Config `log4.properties` file `log4j.appender.file.File` File path
+* Run `Database.sql` on mysql DB
+* Create Ideabiz App on ideabiz.lk
+* Subscribe for relevent API's (SMS for this sample)
+* Generate `consumer key`, `consumer secret`, `refresh token`, `access token` 
+* add above to newly created table
+* config `database.properties`
+* Deploy project on tomcat or run as meven goal `jetty:run` (this project can run independently with IDE)
+* Send SMS inboud API call to `{serverURL}/sms/inbound`
+
+
+### Sample Inbound SMS Request 
+
+DOC (http://docs.ideabiz.lk/en/APIs/SMS)
+
+Send  `POST` Bellow request
+
+URL 
+```
+{serverURL}/sms/inbound
+```
+
+BODY
+```
+{
+  "inboundSMSMessageNotification": {
+    "inboundSMSMessage": {
+      "dateTime": "",
+      "destinationAddress": "tel:87711",
+      "messageId": "113883",
+      "message": "My message",
+      "senderAddress": "tel:+94777123456"
+    }
+  }
+}
+```
---
+++ src/main/java/lk/dialog/ideabiz/api/sampleapp/MainController.java
@@ -0,0 +1,82 @@
+package lk.dialog.ideabiz.api.sampleapp;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lk.dialog.ideabiz.api.model.common.sms.Inbound.InboundSMSRequestWrap;
+import lk.dialog.ideabiz.api.model.common.sms.Outbound.OutboundSMSMessageRequest;
+import lk.dialog.ideabiz.api.model.common.sms.Outbound.OutboundSMSMessagingRequestWrap;
+import lk.dialog.ideabiz.library.APIHandler.SMSHandler;
+import lk.dialog.ideabiz.library.APIHandler.model.SMS.SMSMessage;
+import lk.dialog.ideabiz.library.LibraryManager;
+import lk.dialog.ideabiz.library.model.APICall.APICallResponse;
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Created by Malinda_07654 on 2/9/2016.
+ */
+@Controller
+@RequestMapping("/sms/")
+public class MainController {
+
+    public static Gson gson = null;
+    final static Logger logger = Logger.getLogger(MainController.class);
+    SMSHandler smsHandler;
+
+    public MainController() {
+        gson = new GsonBuilder().serializeNulls().create();
+    }
+
+
+    @RequestMapping(value = "inbound", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+    public ResponseEntity<String> inbound(HttpServletRequest request, @RequestBody String body) {
+
+        //Create sms handler if its null
+        if (smsHandler == null)
+            smsHandler = new SMSHandler("https://ideabiz.lk/apicall/smsmessaging/v2/outbound/94777339033/requests", LibraryManager.getApiCall(), 1, "7555", "tel:7555");
+
+        logger.info("Inbound SMS : " + body);
+        //Reading SMS
+        SMSMessage smsMessage = smsHandler.readSMS(body);
+
+        //Sending sms
+        Boolean status;
+        try {
+            status = smsHandler.sendSMS(smsMessage.getSourceAddress(), smsMessage.getMessage());
+        } catch (Exception e) {
+            logger.error(e);
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<String>(gson.toJson(status), HttpStatus.OK);
+
+    }
+
+    @RequestMapping(value = "sendSMS", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public ResponseEntity<String> inbound(HttpServletRequest request,@RequestParam String msisdn,@RequestParam String message) {
+
+        if (smsHandler == null)
+            smsHandler = new SMSHandler("https://ideabiz.lk/apicall/smsmessaging/v2/outbound/94777339033/requests", LibraryManager.getApiCall(), 1, "7555", "tel:7555");
+
+        logger.info("Send SMS : " + message);
+
+        Boolean status;
+        try {
+            status = smsHandler.sendSMS("tel:+" + msisdn, message);
+        } catch (Exception e) {
+            logger.error(e);
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<String>(gson.toJson(status), HttpStatus.OK);
+
+    }
+
+}
